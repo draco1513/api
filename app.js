ddbcc2b2ef2745ed5997ff683eecdff8e911e748
draco1513 <51796955+draco1513@users.noreply.github.com>
2023-05-12 08:21:58 -0500

    ssssss

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.post('/personas', async (req, res) => {
         const { nombres, apepaterno, apematerno, documento, fechanacimiento, email, direccion, idregion, idprovincia, iddistrito } = req.body;
         const result = await pool.query(
             'INSERT INTO persona (nombres, apepaterno, apematerno, documento, fechanacimiento, email, direccion, idregion, idprovincia, iddistrito) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9,$10) RETURNING *',
-            [nombres, apepaterno, apematerno, documento, fechanacimiento,email,direccion,idregion,idprovincia,iddistrito]
+            [nombres, apepaterno, apematerno, documento, fechanacimiento, email, direccion, idregion, idprovincia, iddistrito]
         );
         res.status(201).json(result.rows[0]);
     } catch (err) {
@@ -58,10 +58,10 @@ app.post('/personas', async (req, res) => {
 app.put('/personas/:id', async (req, res) => {
     try {
         const { id } = req.params;
-        const { nombres, apepaterno, apematerno ,documento ,fechanacimiento ,email ,direccion ,idregion ,idprovincia ,iddistrito} = req.body;
+        const { nombres, apepaterno, apematerno, documento, fechanacimiento, email, direccion, idregion, idprovincia, iddistrito } = req.body;
         const result = await pool.query(
             'UPDATE persona SET nombres = $1 ,apepaterno =$2 ,apematerno =$3 ,documento =$4 ,fechanacimiento =$5 ,email =$6 ,direccion =$7 ,idregion =$8 ,idprovincia =$9 ,iddistrito =$10 WHERE idpersona = $11 RETURNING *',
-            [nombres ,apepaterno ,apematerno ,documento ,fechanacimiento,email,direccion,idregion,idprovincia,iddistrito,id]
+            [nombres, apepaterno, apematerno, documento, fechanacimiento, email, direccion, idregion, idprovincia, iddistrito, id]
         );
         if (result.rowCount === 0) {
             res.status(404).json({ error: 'Persona no encontarda' });
@@ -73,19 +73,19 @@ app.put('/personas/:id', async (req, res) => {
     }
 });
 
-app.delete('/personas/:id', async (req,res)=>{
-    try{
-        const {id} = req.params;
-        const result = await pool.query('DELETE FROM persona WHERE idpersona=$1',[id]);
-        if(result.rowCount===0){
-            res.status(404).json({error:'Persona no encontarda'});
-        }else{
+app.delete('/personas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('DELETE FROM persona WHERE idpersona=$1', [id]);
+        if (result.rowCount === 0) {
+            res.status(404).json({ error: 'Persona no encontarda' });
+        } else {
             res.status(200).json({ message: 'Persona eliminada' });
         }
-    }catch(err){
-        res.status(500).json({error:err.message});
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
- });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port ${port}`));
